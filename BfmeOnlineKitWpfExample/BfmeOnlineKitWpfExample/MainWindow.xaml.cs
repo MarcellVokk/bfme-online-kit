6c8d0b642a3a85849677aa6f2184ada8a0aecf16
Gazdag Marcell <40302952+MarcellVokk@users.noreply.github.com>
2024-08-10 17:53:32 +0200

    Add files via upload

--- BfmeOnlineKitWpfExample/BfmeOnlineKitWpfExample/MainWindow.xaml.cs
+++ BfmeOnlineKitWpfExample/BfmeOnlineKitWpfExample/MainWindow.xaml.cs
@@ -1,21 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace BfmeOnlineKitWpfExample
 {
@@ -31,7 +14,7 @@ namespace BfmeOnlineKitWpfExample
 
         private void LoadClicked(object sender, RoutedEventArgs e)
         {
-            online_menu.Load(game: BFMECompetetiveArena_OnlineKit.Data.BfmeGame.BFME1);
+            online_menu.Load(game: BfmeFoundationProject.OnlineKit.Data.BfmeGame.BFME1);
         }
 
         private void UnloadClicked(object sender, RoutedEventArgs e)
